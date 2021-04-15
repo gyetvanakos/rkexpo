573059a7075e944ca5f3fb133f14c7ace2f25bf3
sztivi01 <56344812+sztivi01@users.noreply.github.com>
2021-04-15 14:24:24 +0200

    piros lett a gomb geci

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
@@ -22,7 +22,7 @@
 
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: red;
 
     &.router-link-exact-active {
       color: #42b983;
