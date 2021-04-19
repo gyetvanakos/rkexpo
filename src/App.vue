aeb85ab2ddae8b3f9f3e52c00522ef281d4bbf97
sztivi01 <56344812+sztivi01@users.noreply.github.com>
2021-04-19 15:23:12 +0200

    routing
    igen

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <router-link to="/References">References</router-link>
+      <router-link to="/Calculator">Calculator</router-link>
+      <router-link to="/Contact">Contact</router-link>
     </div>
     <router-view />
   </div>
---
+++ src/views/Contact.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>This is a Contact us page</h1>
+  </div>
+</template>
