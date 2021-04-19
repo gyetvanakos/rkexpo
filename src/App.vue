57645634af0d037ab1b5b20fdad3175f42f0685b
gyetvanakos <gyetvanakos@gmail.com>
2021-04-19 15:31:47 +0200

    asd
    asd

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       <router-link to="/about">About</router-link>
       <router-link to="/References">References</router-link>
       <router-link to="/Calculator">Calculator</router-link>
-      <router-link to="/Contact">Contact</router-link>
+      <router-link to="/Contact">Contact</router-link>asd
     </div>
     <router-view />
   </div>
