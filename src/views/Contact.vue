4a9c7e4ec5d132846179878c2b9fbf6666bd4a0a
klar2161 <56342242+klar2161@users.noreply.github.com>
2021-05-12 17:50:02 +0200

    hello

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="about">
-    <h1>This is a Contact us page</h1>
+  <div class="contact-us">
+    <h1 id = "headline">This is a Contact us page</h1>
   </div>
 </template>
+
+
+<style lang="scss">
+
+.contact-us
+{
+  width: 100px;
+  height: 400px;
+  background-color: red;
+}
+
+#headline{
+  padding-top: 100px;
+  color: aqua;
+  padding-left: 100px
+}
+
+</style>
